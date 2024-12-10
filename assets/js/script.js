3a536faa8d812830140988d3188c8d81559e0dfb
SarahCharef <charef.sarah@gmx.fr>
2024-12-10 16:00:12 +0100

    push push

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,49 +1,32 @@
-document.getElementById('logo').onclick = () => {
-  window.location.href = 'index.html'; // Redirige vers la page d'accueil
+
+
+
+
+document.getElementById("logo").onclick = () => {
+  window.location.href = "index.html";
 };
 
-const slider = document.getElementById('slider');
-const slides = slider.getElementsByClassName('slide');
-const prevButton = document.getElementById('prev');
-const nextButton = document.getElementById('next');
-const dots = document.querySelectorAll('.dot');
+const slider = document.getElementById("slider");
+const slides = slider.getElementsByClassName("slide");
+const prevButton = document.getElementById("prev");
+const nextButton = document.getElementById("next");
+const dots = document.querySelectorAll(".dot");
 
 let currentIndex = 0;
 
 function updateSlide(index) {
   slider.style.transform = `translateX(-${index * 100}%)`;
   dots.forEach((dot, i) => {
-    dot.classList.toggle('active', i === index);
+    dot.classList.toggle("active", i === index);
   });
 }
 
-prevButton.addEventListener('click', () => {
-  currentIndex = (currentIndex > 0) ? currentIndex - 1 : slides.length - 1;
+prevButton.addEventListener("click", () => {
+  currentIndex = currentIndex > 0 ? currentIndex - 1 : slides.length - 1;
   updateSlide(currentIndex);
 });
 
-nextButton.addEventListener('click', () => {
-  currentIndex = (currentIndex < slides.length - 1) ? currentIndex + 1 : 0;
+nextButton.addEventListener("click", () => {
+  currentIndex = currentIndex < slides.length - 1 ? currentIndex + 1 : 0;
   updateSlide(currentIndex);
 });
-
-
-window.addEventListener('load', () => {
-
-  document.body.style.overflow = 'hidden';
-
-
-  setTimeout(() => {
-
-    const loader = document.getElementById('loader');
-    loader.classList.add('swipeUp');
-  }, 4000);
-
-
-  setTimeout(() => {
-
-    document.body.style.overflow = 'auto';
-  }, 5000);
-});
-
-
